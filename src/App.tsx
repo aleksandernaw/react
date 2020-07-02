94747cd2ce416b0eb178cf4a6d3c920b071eaae8
Radoslaw Wylot <radoslaw.wylot@nokia.com>
2020-07-02 22:33:56 +0200

    dodane propsy, przekierowanie do App po podaniu hasla

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface AppProps {
+  name: string;
+}
+
+const App: React.FC<AppProps> = (props) => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome {props.name}</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -3,33 +3,44 @@ import { ENGINE_METHOD_PKEY_ASN1_METHS } from 'constants';
 import { getPackedSettings } from 'http2';
 import { PassThrough } from 'stream';
 import './Hello.css';
+import App from '../App';
 export const Hello = () => {
-    const minLength = 4
-    const maxLength = 10
-    const [value, setValue] = useState('') 
-    const valueLength: number = value.length;
-    const password: string = 'olek123'
-    const conditionalText: string= value ===password ? 'poprawne hasło' : 'niepoprawne hasło';
-    const isValueLengthInRange = (val: number, lower: number, upper: number) =>{
-        if (val > lower && val< upper) {
-            return true
-        } else {
-            return false
-        }
+  const [value, setValue] = useState('') 
+  
+  const minLength = 4
+  const maxLength = 10
+  const valueLength: number = value.length;
+  const username = 'Olsen';
+  const password: string = 'olek123'
+  const isPasswordCorrect = (value: string, password: string) => {
+    return value === password;
+  };
+
+  const isValueLengthInRange = (val: number, lower: number, upper: number) =>{
+    if (val > lower && val< upper) {
+      return true
+    } else {
+      return false
     }
-    const inputColor = isValueLengthInRange(valueLength, minLength, maxLength) ? 'valid' : 'invalid';
-    return(
+  }
+  
+  const conditionalText: string= isPasswordCorrect(value, password) ? 'poprawne hasło' : 'niepoprawne hasło';
+  const inputColor = isValueLengthInRange(valueLength, minLength, maxLength) ? 'valid' : 'invalid';
+
+  const inputForPassword = (
     <div>
-        <input 
-            className={inputColor}
-            type="password"
-            placeholder="wpisz hasło"
-            value={value}
-            onChange={e => setValue(e.target.value)}>
-        </input>
-    <p id='haslo'>{conditionalText}</p>
-    <p>{valueLength}</p>
+      <input 
+        className={inputColor}
+        type="password"
+        placeholder="wpisz hasło"
+        value={value}
+        onChange={e => setValue(e.target.value)}>
+      </input>
     </div>
+  )
+
+  return (
+    isPasswordCorrect(value, password) ? <App name={username}/> : inputForPassword
 )
 
 }
